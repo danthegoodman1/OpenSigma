ab3dbc295aefe4a2e719b912cd5282752a4f794b
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-03-12 19:37:12 -0400

    fix vitest to work with duckdb

---
+++ src/storage/providers/duckdb/duckdb.test.ts
@@ -0,0 +1,23 @@
+// Anything other than .exec crashes vitest
+
+import { DuckDBProvider } from "./index.js"
+import { test } from "vitest"
+
+test("duckdb", async () => {
+  const str = new DuckDBProvider()
+  await str.Init()
+  await str.InsertEvents([
+    {
+      data: {
+        id: "testid",
+        hey: "ho",
+      },
+      event_type: "test",
+      object_type: "accounts",
+      time_sec: new Date().getTime() / 1000,
+    },
+  ])
+  await str.db.all("select * from stripe_events", (err, rows) => {
+    console.log(rows)
+  })
+})
